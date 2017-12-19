375a4243fdb6d7b5413c852491fa00f3643c9fd6
Joshua Huang <joshua.huang@warriorlife.net>
2017-12-19 10:13:57 -0800

    Added Factorial and Factorial Variable
    *Finished factorial function
    *Began work on factorial using variables

--- calculator.js
+++ calculator.js
@@ -32,3 +32,24 @@ function q_button() {
 		e();
 		d(q(document.getElementById("d").value));	
 }
+
+//Square the Value
+function s(val) {
+	return val *= val;
+}
+function s_button() {
+	e();
+	d(s(document.getElementById("d").value));
+}
+//Evaluate Value with Exponent
+function se(val1,val2) {
+	return val1 *= val2	
+}
+function se_button() {
+	e();
+	var x = document.getElementById("d").value;
+	d("");
+	alert("Please input the value of your exponent");
+	var y = document.getElementById("d").value;
+	d(se(x,y));
+}
